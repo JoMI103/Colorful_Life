e918f1318785540e9d8d18723db6a09dab6a7037
Venâncio <joaovitorvenancio99@outlook.com>
2024-01-20 12:05:12 +0000

    Ui manager shows current magic image

--- Colorful_Life_Project/Assets/Venancio/Scripts/UI/UIManager.cs
+++ Colorful_Life_Project/Assets/Venancio/Scripts/UI/UIManager.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Canvas HUD;
+    [SerializeField] private Image currentMagicImage;
+    [SerializeField] private MagicDictionary[] magicImageDictionary;
+    [SerializeField]
+    private UIManager.Magic CurrentMagic
     {
-        
+        get
+        {
+            return CurrentMagic;
+        }
+        set
+        {
+            MagicChanged = true;
+            CurrentMagic = value;
+        }
     }
 
-    // Update is called once per frame
+    private bool MagicChanged = false;
+
+    [Serializable]
+    private struct MagicDictionary
+    {
+        public UIManager.Magic Magic;
+        public Image Image;
+    }
+
     void Update()
     {
-        
+        UpdateHUD();
     }
+
+    private void UpdateHUD()
+    {
+        UpdateCurrentMagic();
+    }
+
+    private void UpdateCurrentMagic()
+    {
+        if (MagicChanged)
+        {
+            MagicChanged = false;
+
+            foreach (MagicDictionary dictionaryPage in magicImageDictionary)
+            {
+                if (dictionaryPage.Magic.Equals(CurrentMagic))
+                {
+                    currentMagicImage.sprite = dictionaryPage.Image.sprite;
+                    return;
+                }
+            }
+
+            Debug.Log("UIManager: No image found. Check gameobject configuration.");
+        }
+    }
+
+    public void  EnableHUD (bool enable)
+    {
+        HUD.gameObject.SetActive(enable);
+    }
+
+    public enum Magic
+    {
+        Angry,
+        Guilt,
+        Sadness
+    }
+
 }
