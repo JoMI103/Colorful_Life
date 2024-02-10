e89073a5c3ca1a6a22f7763f88ed20bc449511d0
Venâncio <joaovitorvenancio99@outlook.com>
2024-02-10 01:09:04 +0000

    pong 2

--- Colorful_Life_Project/Assets/Venancio/Scripts/FloatObject.cs
+++ Colorful_Life_Project/Assets/Venancio/Scripts/FloatObject.cs
@@ -1,24 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FloatObject : MonoBehaviour
 {
+    [Header("Setup")]
     [SerializeField]
     private Transform _transform;
     [SerializeField]
-    private float _plusYPosition;
+    private float _frequency;
     [SerializeField]
-    private float _minusYPosition;
+    private float _amplitude;
+    [SerializeField]
+    private Vector3 _initialPosition;
+    [Header("Go to first Location Configuration")]
     [SerializeField]
     private bool _isGoingToStartLocation;
     [SerializeField]
     private Transform _startLocation;
+    [SerializeField]
+    private float _translationVelocyToStartLocation;
+    [SerializeField]
+    private float _distanteBetweenStartLocationAnTarget = 2e-05f;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
+        if (_isGoingToStartLocation)
+        {
+            _initialPosition = _startLocation.position;
+            
+        } else
+        {
+            _initialPosition = _transform.position;
+        }
         
     }
 
@@ -26,9 +41,61 @@ public class FloatObject : MonoBehaviour
     void Update()
     {
         if (_isGoingToStartLocation &&
-            !_transform.position.Equals(_startLocation))
+            Vector3.Distance(_transform.position, _startLocation.position) > _distanteBetweenStartLocationAnTarget)
         {
-
+            _startLocation.position = new Vector3(_initialPosition.x, _initialPosition.y + MathF.Sin(Time.time * _frequency) * _amplitude, _initialPosition.z);
+            _transform.position = Vector3.MoveTowards(_transform.position, _startLocation.position, _translationVelocyToStartLocation * Time.deltaTime);
+        } else
+        {
+            _isGoingToStartLocation = false;
+            _transform.position = new Vector3(_initialPosition.x, _initialPosition.y + MathF.Sin(Time.time * _frequency) * _amplitude, _initialPosition.z);
         }
     }
 }
+
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class FloatObject : MonoBehaviour
+//{
+//    [Header("Setup")]
+//    [SerializeField]
+//    private Transform _transform;
+//    [SerializeField]
+//    private float _frequency;
+//    [SerializeField]
+//    private float _amplitude;
+//    private Vector3 _initialPosition;
+//    [Header("Go to first Location Configuration")]
+//    [SerializeField]
+//    private bool _isGoingToStartLocation;
+//    [SerializeField]
+//    private Transform _startLocation;
+//    [SerializeField]
+//    private float _translationVelocyToStartLocation;
+//    private bool _isOnDestination = false;
+
+//    private void Awake()
+//    {
+//        _initialPosition = _transform.position;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (_isGoingToStartLocation &&
+
+//            !_isOnDestination)
+//        {
+
+//            _transform.position = Vector3.MoveTowards(_transform.position, _startLocation.position, _translationVelocyToStartLocation * Time.deltaTime);
+//            if (Vector3.Distance(_transform.position, _startLocation.position) <= 0.5f) _isOnDestination = true;
+//        }
+//        else
+//        {
+//            _transform.position = new Vector3(_initialPosition.x, MathF.Sin(Time.time * _frequency) * _amplitude, _initialPosition.z);
+//        }
+//    }
+//}
