8abcda2edef82f148757d6f27c07901ac5098346
Venâncio <joaovitorvenancio99@outlook.com>
2024-02-10 10:34:30 +0000

    pong

--- Colorful_Life_Project/Assets/_GameFolder/Scripts/DialogueSystem/DialogueManager.cs
+++ Colorful_Life_Project/Assets/_GameFolder/Scripts/DialogueSystem/DialogueManager.cs
@@ -5,6 +5,7 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -16,6 +17,12 @@ public class DialogueManager : MonoBehaviour
     [SerializeField]
     private SpeechGameObjectDictionaryPage[] _speechGameObjectDictionaryPages;
     private Dictionary<string, SpeechGameObjectDictionaryPage> _speechGameObjectDictionary = new Dictionary<string, SpeechGameObjectDictionaryPage>();
+
+    public UnityEvent OnDialogueStartEvent;
+    public UnityEvent OnDialogueEndEvent;
+
+    public OnDialogueStartSpeech OnDialogueStartEventWithSpeech;
+    public OnDialogueEndSpeech OnDialogueEndEventWithSpeech;
 
 
     [Serializable]
@@ -40,6 +47,10 @@ public class DialogueManager : MonoBehaviour
     public void StartDialogue(Speech speech, GameObject dialogueObject, TMPro.TMP_Text dialogueText, TMPro.TMP_Text speakerName)
     {
         if (speech == null)
+        {
+            Debug.Log("Dialogue Manager on StartDialogue: no speech input.");
+            return;
+        }
 
         this._currentSpeech = speech;
         this._dialogueObject = dialogueObject;
@@ -51,6 +62,8 @@ public class DialogueManager : MonoBehaviour
 
         this._nextSpeech = speech;
 
+        OnDialogueStartEvent.Invoke();
+        OnDialogueStartEventWithSpeech.Invoke(_currentSpeech);
         NextDialogue();
 
     }
@@ -105,9 +118,14 @@ public class DialogueManager : MonoBehaviour
 
     public void EndDialogue()
     {
+        OnDialogueEndEvent.Invoke();
+        OnDialogueEndEventWithSpeech.Invoke(_currentSpeech);
         _dialogueObject.SetActive(false);
 
         Debug.Log("TERMINOU");
     }
 
 }
+
+[Serializable] public class OnDialogueStartSpeech : UnityEvent<Speech> { }
+[Serializable] public class OnDialogueEndSpeech : UnityEvent<Speech> { }
