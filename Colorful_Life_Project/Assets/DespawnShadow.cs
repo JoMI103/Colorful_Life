07946c24bf55481d0c9474f169e1bccdc5baeada
Venâncio <joaovitorvenancio99@outlook.com>
2024-02-17 13:13:35 +0000

    SADAS

---
+++ Colorful_Life_Project/Assets/DespawnShadow.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnShadow : MonoBehaviour
+{
+    public void DespawnAShadow(Speech speech)
+    {
+    }
+}
